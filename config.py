ba6e3e6ea61d917436e6153c58ee29e45596a982
Boguslaw Witkowski <vibowit@vibowit.com>
2015-11-13 11:53:22 +0100

    added db maintanance

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -7,3 +9,10 @@ OPENID_PROVIDERS = [
         {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
         {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
         {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+# SQLAlchemy config
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
