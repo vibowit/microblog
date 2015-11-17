b1d4cb87941077d0ae06992bdebe774bf063167f
vibowit <vibowit@vibowit.com>
2015-11-17 09:04:05 +0100

    nickname uniqueness and test framework

--- app/models.py
+++ app/models.py
@@ -10,17 +10,21 @@ class User(db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
 
+
     @property
     def is_authenticated(self):
         return True
+
 
     @property
     def is_active(self):
         return True
 
+
     @property
     def is_anonymous(self):
         return False
+
 
     def get_id(self):
         try:
@@ -28,12 +32,27 @@ class User(db.Model):
         except NameError:
             return str(self.id) # python 3
 
+
     def avatar(self, size):
         return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
 
 
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname=nickname).first() is None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            version += 1
+        return new_nickname
+
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
