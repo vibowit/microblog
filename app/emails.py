d10cc4f1d1d5a15dabe3c363b8b5390dc6296696
vibowit <vibowit@vibowit.com>
2015-11-20 08:25:05 +0100

    added threading with async decorator

--- app/emails.py
+++ app/emails.py
@@ -1,14 +1,21 @@
+from decorators import async
 from flask import render_template
 from flask.ext.mail import Message
+from app import app
 
 from app import mail
 from config import ADMINS
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    send_async_email(app, msg)
 
 def follower_notification(followed, follower):
     send_email("[microblog] %s is now following you!" % follow.nickname,
